71b0f32b175c355da0ef2fdc91d870186c8c5af0
AayishaShaji <aayishashaji14@gmail.com>
2025-08-09 05:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Flask looks inside /templates
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
+
